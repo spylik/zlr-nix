1e2a19c2f39911bf26407ded4d4da175e30d6e1e
JenkinsDutyServer <jenkins@duty.kiev.ua>
2016-12-30 13:04:08 +0200

    early draft

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+let 
+	stdenv = pkgs.stdenv;
+in rec {
+	zlr-nix = stdenv.mkDerivation rec {
+		name = "zlr-nix";
+		src = fetchgit {
+			url = "git://github.com/spylik/zlr.git";
+			rev = "8e3403f403e65647200583968fe835478dfeda5c";
+		};
+		BuildInputs = [
+			"pkgs.erlang-19.1.6"
+		];
+		builder = builtins.toFile "builder.sh" "make"; 
+	};
+}
