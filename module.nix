4c3458569ee1cf1f884b1c37bf588ad8e2f28db6
Oleksii Semilietov <spylik@gmail.com>
2017-01-04 13:26:39 +0000

    fix module definition

--- module.nix
+++ module.nix
@@ -1,10 +1,16 @@
 {config, pkgs, ...}:
 
+let 
+    # extend packages with our own package
+    extpkg = pkgs.callPackage ./package.nix {};
+in
+
 {
+    # creating system.d service
     systemd.services.zaloraWWW = {
         description = "Start zalora web service.";
         wantedBy = [ "multi-user.target" ];
         after = [ "network.target" ];
-        serviceConfig.ExecStart = ''${pkgs.zlr}/_rel/zlr/bin/zlr start'';
+        serviceConfig.ExecStart = ''${extpkg.zlr}/bin/zlr start'';
     };
 }
